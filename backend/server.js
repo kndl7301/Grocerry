040c458a5b2794f7785b293eef128d8374d9a8a8
unknown <kendalyuce7301@gmail.com>
2025-06-19 09:42:46 +0300

    changing the cors settings

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://grocerry-1.onrender.com"],
     credentials: true,
   })
 );
